028f745d8b8c953af6af4fc1d414724998367641
Chris Lopez <44450518+loChris@users.noreply.github.com>
2025-09-01 12:27:54 -0400

    adding coyote jump

--- player.gd
+++ player.gd
@@ -5,23 +5,25 @@ const FRICTION = 1000
 const SPEED = 100.0
 const JUMP_VELOCITY = -300.0
 
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var coyote_jump_timer: Timer = $CoyoteJumpTimer
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# handle gravity
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += gravity * delta
 
-	# Handle jump.
-	if is_on_floor():
-		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+ # handle jump
+	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
+		if Input.is_action_just_pressed("ui_accept"):
 			velocity.y = JUMP_VELOCITY
-	else:
+	if not is_on_floor():
 		if Input.is_action_just_released("ui_accept") and velocity.y < JUMP_VELOCITY / 2:
 			velocity.y = JUMP_VELOCITY / 2
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# handle lateral movement
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = move_toward(velocity.x, SPEED * direction, ACCELERATION * delta)
@@ -29,7 +31,13 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
 
 	update_animations(direction)
+
+	# handle coyote jump
+	var was_on_floor = is_on_floor()
 	move_and_slide()
+	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
+	if just_left_ledge:
+		coyote_jump_timer.start()
 
 func update_animations(input_axis):
 	if input_axis != 0:
