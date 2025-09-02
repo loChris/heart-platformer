563c1b4002949fb812423fc1f77ec75293ec4b65
Chris Lopez <44450518+loChris@users.noreply.github.com>
2025-09-01 20:58:37 -0400

    adding wall jump and double jumping

--- PlayerMovementData.gd
+++ PlayerMovementData.gd
@@ -1,9 +1,10 @@
 class_name PlayerMovementData
 extends Resource
 
-@export var acceleration: int = 1000
-@export var friction: int = 1000
+@export var acceleration: float = 1000.0
+@export var friction: float = 1000.0
 @export var speed: float = 100.0
 @export var jump_velocity: float = -250.0
 @export var gravity_scale: float = 0.8
 @export var air_resistance: float = 1.0
+@export var air_acceleration: float = 400.0
